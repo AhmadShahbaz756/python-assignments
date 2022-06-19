c3d3ec59dd7c83d4aeb9aa0949fac4710b7c880b
unknown <Ahmad>
2022-06-19 16:31:47 +0500

    assignments

---
+++ main .py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://lms.umt.edu.pk/login/index.php")
+driver.find_element_by_id("username").send_keys("F2019065220")
+driver.find_element_by_id("password").send_keys("dEbHnU3@")
+driver.find_element_by_id("loginbtn").click()
+Sub = driver.find_elements_by_class_name("list-group-item list-group-item-action  ")
+subject_list = []
+for s in range(len(Sub)):
+    print(subject_list.append(Sub[s].text))
---
+++ pythonProject/pythonProject1/assignment_six/ahmad/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+class AhmadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name='ahmad'
+
+    # def ready(self):
+
+
+
+
